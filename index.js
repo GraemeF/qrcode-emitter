28c268a18fce0ac2dd3bcc1d29d4851d91edfdb5
Graeme Foster <graemef@gmail.com>
2012-11-11 08:04:57 +0000

    Discard images that we don't have time to process.

--- index.js
+++ index.js
@@ -4,23 +4,37 @@ var qrcode = require('jsqrcode')(Canvas);
 var util = require("util");
 var events = require("events");
 
-var QR = function (pngStream) {
+var QR = function (images) {
   events.EventEmitter.call(this);
-  this.pngStream = pngStream;
+  this.images = images;
 };
 
 util.inherits(QR, events.EventEmitter);
 
 QR.prototype.start = function () {
-  this.pngStream.on('data', scanForCode);
-
   var self = this;
-  function scanForCode(image) {
-    try {
-      var result = qrcode.decode(image);
-      self.emit('qrcode', result);
-    } catch (e) {
+  var queuedImage = null;
+
+  this.images.on('data', queueImageForProcessingIfIdle);
+
+  function queueImageForProcessingIfIdle(image) {
+    // only queue an image for scanning if there isn't one waiting
+    // in order to avoid building up lag if we can't keep up
+    if (queuedImage === null) {
+      queuedImage = image;
+
+      // scan the image *after* any more images arrive
+      process.setTimeout(scanForCode, 0);
     }
+  }
+
+  function scanForCode() {
+    try {
+      self.emit('qrcode', qrcode.decode(queuedImage));
+    } catch (e) {
+      // probably didn't find a code
+    }
+    queuedImage = null;
   }
 };
 
